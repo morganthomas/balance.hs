2c77d2b192b6fc4a86e09734f8834c0655d4b647
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2020-11-16 20:23:24 -0500

    partially rigid fill elements

--- src/Balance/Element/Fill.hs
+++ src/Balance/Element/Fill.hs
@@ -6,6 +6,10 @@
 module Balance.Element.Fill
   ( FillElement (..)
   , FillElementParams (..)
+  , fillHorizontallyRigid
+  , fillHorizontallyVeryRigid
+  , fillVerticallyRigid
+  , fillVerticallyVeryRigid
   , fillRigid
   , fillVeryRigid
   , fillFlex
@@ -48,7 +52,36 @@ instance RectangularElement (FillElement a) where
   boundingBox _ = lens unFillElementParams (const FillElementParams)
 
 
--- TODO: rigid in one direction and flex in another
+horizontallyRigidError :: ( Mode a, Scalar a ~ Double, Reifies s Tape )
+                     => Width Double -> FillElementParams (Reverse s a) -> Error (Reverse s a)
+horizontallyRigidError ideal (FillElementParams rect) =
+  Error . abs . unLength . unWidth $ (auto . auto <$> ideal) - widthDim (rectangleDimensions rect)
+
+
+fillHorizontallyRigid :: ( Mode a, Scalar a ~ Double )
+                      => PenaltyFn a -> Width Double -> Maybe (Colour Double) -> FillElement a
+fillHorizontallyRigid pen ideal = FillElement (pen . horizontallyRigidError ideal)
+
+
+fillHorizontallyVeryRigid :: ( Mode a, Scalar a ~ Double )
+                          => Width Double -> Maybe (Colour Double) -> FillElement a
+fillHorizontallyVeryRigid = fillHorizontallyRigid (quadraticPenalty prohibitiveQuadraticPenalty)
+
+
+verticallyRigidError :: ( Mode a, Scalar a ~ Double, Reifies s Tape )
+                     => Height Double -> FillElementParams (Reverse s a) -> Error (Reverse s a)
+verticallyRigidError ideal (FillElementParams rect) =
+  Error . abs . unLength . unHeight $ (auto . auto <$> ideal) - heightDim (rectangleDimensions rect)
+
+
+fillVerticallyRigid :: ( Mode a, Scalar a ~ Double )
+                    => PenaltyFn a -> Height Double -> Maybe (Colour Double) -> FillElement a
+fillVerticallyRigid pen ideal = FillElement (pen . verticallyRigidError ideal)
+
+
+fillVerticallyVeryRigid :: ( Mode a, Scalar a ~ Double )
+                        => Height Double -> Maybe (Colour Double) -> FillElement a
+fillVerticallyVeryRigid = fillVerticallyRigid (quadraticPenalty prohibitiveQuadraticPenalty)
 
 
 rigidError :: ( Mode a, Scalar a ~ Double, Reifies s Tape )
