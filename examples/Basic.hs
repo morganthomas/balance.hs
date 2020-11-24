e1f4e6f1d1523493f233f16bf13c9a130aa00d6f
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2020-11-23 20:27:16 -0500

    basic example fills canvas

--- examples/Basic.hs
+++ examples/Basic.hs
@@ -7,7 +7,7 @@ module Main where
 import Balance.Element
 import Balance.Element.Fill
 import Balance.Element.Std
-import Balance.Geometry
+import Balance.Surface
 import Balance.Surface.BlankCanvas
 
 import Control.Monad (forM_)
@@ -27,9 +27,12 @@ options = Options port [] False "." [] False
 main :: IO ()
 main = do
   putStrLn $ "starting balance.hs basic example on port " <> show port
-  let el :: StdEl Double = Fix . FillEl . fillVeryRigid (Dimensions 100 100) $ Just (sRGB 0.0 0.0 0.0)
-      approximations = optimize el
-      maxIterations = 100
   blankCanvas options $ \ctx -> do
     putStrLn "opened a canvas"
-    send ctx $ forM_ (take maxIterations approximations) $ \params -> render el params (BlankCanvasSurface ctx)
+    send ctx $ do
+      let surface = BlankCanvasSurface ctx
+      dims <- getSurfaceDimensions surface
+      let el :: StdEl Double = Fix . FillEl . fillVeryRigid dims $ Just (sRGB 0 0 0)
+          approximations = optimize el
+          maxIterations = 100
+      forM_ (take maxIterations approximations) $ \params -> render el params surface
